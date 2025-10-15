9a0da898e9a690ef87c7a8f42eb951df7b15f374
Fabricio Nicolas Duarte <fabricioduarteoficial@gmail.com>
2025-10-15 04:02:07 -0300

    Cambios en Dockerfile y app.php.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\TrustProxies;
+use Illuminate\Http\Request; // <-- Importamos la clase Request
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,17 +12,18 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // --- CONFIGURACIÓN CORREGIDA ---
-        // Esta es la sintaxis correcta y compatible para confiar en los proxies de Render.
+        // --- CONFIGURACIÓN CORRECTA Y DEFINITIVA ---
+        // Le decimos a Laravel que confíe en todos los proxies de Render
+        // usando las constantes correctas de la clase Request.
         $middleware->trustProxies(
             '*', // El primer argumento es la lista de proxies
-            TrustProxies::HEADER_X_FORWARDED_FOR |
-            TrustProxies::HEADER_X_FORWARDED_HOST |
-            TrustProxies::HEADER_X_FORWARDED_PORT |
-            TrustProxies::HEADER_X_FORWARDED_PROTO |
-            TrustProxies::HEADER_X_FORWARDED_AWS_ELB
+            Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_AWS_ELB
         );
-        // -----------------------------
+        // ------------------------------------------
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
